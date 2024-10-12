332d322e6cc3de9e3bc1db624a0bf9b3e933879a
azkia ajmal fairuz <azkiajmal@gmail.com>
2024-10-12 18:34:08 +0700

    helper write json

--- broker-service/cmd/api/helpers.go
+++ broker-service/cmd/api/helpers.go
@@ -31,3 +31,27 @@ func (app *Config) ReadJson(w http.ResponseWriter, r *http.Request, data any) er
 	}
 	return nil
 }
+
+func (app *Config) WriteJson(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key,value := range headers[0] {
+
+			w.Header()[key] = value
+			
+		}
+	}
+
+	w.Header().Set("Content-Type","applications/json")
+	w.WriteHeader(status)
+	_,err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
